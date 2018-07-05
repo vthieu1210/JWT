085053db1952965f7ce3e455d29d38cb26e6fd56
vthieu1210 <40747463+vthieu1210@users.noreply.github.com>
2018-07-05 14:54:07 +0700

    Initial commit

---
+++ Register/Controllers/GenerateTokenController.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using Register.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Results;
+using Jose;
+
+namespace Register.Controllers
+{
+    public class GenerateTokenController : ApiController
+    {
+        [HttpGet]
+        public String GenerateToken()
+        {
+            ConsumerJWT consumerJwt = new ConsumerJWT();
+            var headers = Request.Headers;
+            try
+            {
+                string[] authorization = headers.GetValues("Authorization").First().Split(' ');
+                string encodedString = authorization[1];
+                byte[] data = Convert.FromBase64String(encodedString);
+                string decodedString = Encoding.UTF8.GetString(data);
+                consumerJwt.key = decodedString.Split(':')[0];
+
+                string secret = consumerJwt.getSecret();
+                if (secret == null)
+                    return "Consumer not found! (Invalid username or wrong syntax)";
+
+                var secretKey = Encoding.UTF8.GetBytes(secret);
+
+                TimeSpan t = DateTime.UtcNow.AddMinutes(consumerJwt.exp) - new DateTime(1970, 1, 1);
+                int secondsSinceEpoch = (int)t.TotalSeconds;
+
+                var payload = new JwtPayload
+                {
+                    { "iss", consumerJwt.key},
+                    { "exp", secondsSinceEpoch},
+                };
+
+                string tokenString = Jose.JWT.Encode(payload, secretKey, JwsAlgorithm.HS256);
+
+                return tokenString;
+            }
+            catch (Exception e)
+            {
+                return e.GetType().Name + ": " + e.Message;
+            }
+        }
+    }
+}
---
+++ Register/Models/ConsumerJWT.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Register.Models
+{
+    public class ConsumerJWT
+    {
+        public string key { get; set; }
+        public int exp { get; set; }
+        public string getSecret()
+        {
+            string path = HttpContext.Current.Request.PhysicalApplicationPath + "/consumers.txt";
+            string data = "";
+
+            if (File.Exists(path))
+                data = System.IO.File.ReadAllText(@path);
+            string[] consumers = data.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            for (int i = 0; i < consumers.Length; i++) {
+                string[] tmp = consumers[i].Split(' ');
+                if (tmp[0] == this.key)
+                {
+                    if (tmp.Length == 3)
+                        this.exp = Int32.Parse(tmp[2]);
+                    else
+                        this.exp = 5;
+                    return tmp[1];
+                }
+            }
+            return null;
+        }
+    }
+}
